9c5a10cdc22ed5a472a66e77aa0859f25dcd629b
RollyLumibao <blatch.lumibao@gmail.com>
2016-07-12 16:33:42 +0800

    updating

--- main.js
+++ main.js
@@ -2,15 +2,16 @@
 
     var user;
     var messages = [];
+    var $joinchat = $('#joinchat');
+    var $userForm = $('#userForm');
+    var $messageFormArea= $('#messageFormArea');
+    var $messageForm= $('#messageForm');
+    var $Message = $('#Message');
+    
 
-    var messages_template = Handlebars.compile($('#messages-template').html());
 
-    function updateMessages(msg){
-        messages.push(msg);
-        var messages_html = messages_template({'messages': messages});
-        $('#messages').html(messages_html);
-        $("#messages").animate({ scrollTop: $('#messages')[0].scrollHeight}, 1000);
-    }
+
+//Connect to websocket
 
     var conn = new WebSocket('ws://localhost:8080');
     conn.onopen = function(e) {
@@ -22,11 +23,23 @@
         updateMessages(msg);
     };
 
+    var messages_template = Handlebars.compile($('#messages-template').html());
 
-    $('#join-chat').click(function(){
+// chat box update
+    function updateMessages(msg){
+        messages.push(msg);
+        var messages_html = messages_template({'messages': messages});
+        $('#messages').html(messages_html);
+        $("#messages").animate({ scrollTop: $('#messages')[0].scrollHeight}, 1000);
+    }
+
+
+//join chat  
+
+    $joinchat.click(function(){
         user = $('#user').val();
-        $('#user-container').addClass('hidden');
-        $('#main-container').removeClass('hidden');
+        $userForm.addClass('hidden');
+        $messageFormArea.removeClass('hidden');
 
         var msg = {
             'user': user,
@@ -41,7 +54,10 @@
     });
 
 
-    $('#send-msg').click(function(){
+//send chat
+
+    $Message.click(function(){
+
         var text = $('#msg').val();
         var msg = {
             'user': user,
@@ -52,26 +68,6 @@
         conn.send(JSON.stringify(msg));
 
         $('#msg').val('');
-    });
-
-
-    $('#leave-room').click(function(){
-
-        var msg = {
-            'user': user,
-            'text': user + ' has left the room',
-            'time': moment().format('hh:mm a')
-        };
-        updateMessages(msg);
-        conn.send(JSON.stringify(msg));
-
-        $('#messages').html('');
-        messages = [];
-
-        $('#main-container').addClass('hidden');
-        $('#user-container').removeClass('hidden');
-
-        conn.close();
     });
 
 })();
